c8b56ee00f1926768c4c451f2bcd256da9120bb4
Parker Fink <parkeririshgod@gmail.com>
2024-02-18 19:18:34 -0500

    updated PSMS.py
    -Auto installs required modules and if they are already installed- runs program

--- PSMS/PSMS.py
+++ PSMS/PSMS.py
@@ -1,26 +1,57 @@
 import platform
 import os
-
+import json
 
 
 #checks what the current os is
 system = platform.system()
-
 print(system)
+
+
 
 
 #actions based on current os
 if system == "Windows":
-    os.system('pip3 install customtkinter')
-    os.system('pip3 install pynput')
-    os.system('pip3 install plyer')
-    os.system('python PSMS_Client.py')
+     with open('config.json', 'r') as inFile:
+        data = json.load(inFile)
+
+        if data['setup'] == False:
+            os.system('pip install customtkinter')
+            os.system('pip install pynput')
+            os.system('pip install plyer')
+
+            data["setup"] = True
+
+            with open('config.json', 'w') as inFile:
+                json.dump(data, inFile)
+
+            inFile.close()
+            os.system('python PSMS_Client.py')
+    
+        elif data["setup"] == True:
+            os.system('python PSMS_Client.py')
     
 
+
+
 elif system == "Linux":
-    os.system('pip3 install customtkinter')
-    os.system('pip3 install pynput')
-    os.system('pip3 install plyer')
-    os.system('python3 PSMS_Client.py')
-    
+    with open('config.json', 'r') as inFile:
+        data = json.load(inFile)
+
+        if data['setup'] == False:
+            os.system('pip3 install customtkinter')
+            os.system('pip3 install pynput')
+            os.system('pip3 install plyer')
+
+            data["setup"] = True
+
+            with open('config.json', 'w') as inFile:
+                json.dump(data, inFile)
+
+            inFile.close()
+            os.system('python3 PSMS_Client.py')
+
+        elif data["setup"] == True:
+            os.system('python3 PSMS_Client.py')
+
 
